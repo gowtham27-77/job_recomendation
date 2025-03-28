e46c602a71610bda6cfd38a5f264a25878a094b1
Gowtham27 <ganaparapugowtham2@gmail.com>
2025-03-29 00:14:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load the preprocessed recommendation data from the pickle file
-recommendations_df = pd.read_pickle('job_recommendations.pkl')
+# Load the recommendations data
+recommendations_df = pd.read_pickle('recommendations.pkl')
 
 @app.route('/')
 def home():
@@ -13,8 +13,15 @@ def home():
 @app.route('/recommend', methods=['GET'])
 def recommend():
     applicant_id = request.args.get('applicant_id')
+    
+    # Check if the applicant ID is missing
+    if not applicant_id:
+        return jsonify({'error': 'Applicant ID is missing'}), 400
+    
+    # Find recommendations for the given applicant ID
     recommendations = recommendations_df[recommendations_df['applicantId'] == applicant_id]
 
+    # Check if no recommendations were found
     if recommendations.empty:
         return jsonify({'error': 'No recommendations found for this applicant'}), 404
 
